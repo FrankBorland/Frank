ff73ea6dc71282de2ec5aea1259f703578733179
Bernhard Reichl <godot@Bernhards-MacBook-Air.local>
2014-09-12 09:38:37 +0200

    + add longPressWithDuration for switching a UISwitch + add UIButton control to example Controls frankified app

---
+++ example/Controls/Controls/UIButtonViewController.m
@@ -0,0 +1,64 @@
+//
+//  UIButtonViewController.m
+//  Controls
+//
+//  Created by bernhardr
+//
+
+#import "UIButtonViewController.h"
+
+
+@implementation UIButtonViewController
+
+@synthesize reportLabel=_reportLabel;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        self.title = @"UIButton";
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+- (IBAction)buttonPressed:(id)sender {
+    self.reportLabel.text = @"Button 1 pressed";
+}
+- (IBAction)button2Pressed:(id)sender {
+    self.reportLabel.text = @"Button 2 pressed";
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
